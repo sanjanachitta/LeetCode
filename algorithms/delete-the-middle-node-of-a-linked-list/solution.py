67c7c901f945252623807d28fc3f5455618400cd
sanjana chitta <sanjanachitta63@gmail.com>
2026-08-22 22:35:38 +0530

    feat: add Delete the Middle Node of a Linked List solution

--- algorithms/delete-the-middle-node-of-a-linked-list/solution.py
+++ algorithms/delete-the-middle-node-of-a-linked-list/solution.py
@@ -4,25 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        cur = head
-        L = 0
-
-        # Find length
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur=head
+        N=0
         while cur:
-            L += 1
-            cur = cur.next
-
-        # If deleting the first node
-        if L == n:
-            return head.next
-
-        # Go to node BEFORE the node to delete
-        cur = head
-        for i in range(L - n - 1):
-            cur = cur.next
-
-        # Skip the nth node from the end
-        cur.next = cur.next.next
-
+            N+=1
+            cur =cur.next
+        cur=head
+        if N == 1:
+           return None
+        for i in range(N//2-1):
+            cur=cur.next
+        cur.next=cur.next.next
         return head
+
