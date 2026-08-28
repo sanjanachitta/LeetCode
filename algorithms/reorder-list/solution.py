c9456529d19ee6dc114a8f372d2009b1a680c123
sanjana chitta <sanjanachitta63@gmail.com>
2026-08-28 10:40:36 +0530

    feat: add Reorder List solution

---
+++ algorithms/reorder-list/solution.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        # 1. Find the middle
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2. Reverse the second half
+        prev = None
+        cur = slow.next
+        slow.next = None
+
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        # 3. Merge the two halves
+        first = head
+        second = prev
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
