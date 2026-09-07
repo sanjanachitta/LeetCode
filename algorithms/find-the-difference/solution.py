2201653e30a270ce412c11b3e40a5d8948cd781d
sanjana chitta <sanjanachitta63@gmail.com>
2026-09-07 18:09:28 +0530

    feat: add Find the Difference solution

--- algorithms/find-the-difference/solution.py
+++ algorithms/find-the-difference/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        dict = {}
-        for char in s:
-            if char in dict:
-                dict[char]+=1
-            else:
-                dict[char]=1
-        for i in t:
-           dict[i] = dict.get(i, 0) - 1
+        # dict = {}
+        # for char in s:
+        #     if char in dict:
+        #         dict[char]+=1
+        #     else:
+        #         dict[char]=1
+        # for i in t:
+        #    dict[i] = dict.get(i, 0) - 1
 
-           if dict[i] == -1:
-             return i
+        #    if dict[i] == -1:
+        #      return i
+        # for char in t:
+        #    if t.count(char) > s.count(char):
+        #       return char
+        for char in s:
+            t = t.replace(char, '', 1)
+        return t
