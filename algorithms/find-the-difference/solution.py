8cdaf0626715005b66fb32d323082056df7b1d35
sanjana chitta <sanjanachitta63@gmail.com>
2026-09-07 18:03:24 +0530

    feat: add Find the Difference solution

--- algorithms/find-the-difference/solution.py
+++ algorithms/find-the-difference/solution.py
@@ -1,17 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def defs(left,right):
-            if not left and not right:
-                return True
-            if not left or not right :
-                return False
-            return (left.val==right.val and
-                defs(left.left,right.right) and defs(left.right,right.left))
-        return defs(root.left,root.right)
+    def findTheDifference(self, s: str, t: str) -> str:
+        dict = {}
+        for char in s:
+            if char in dict:
+                dict[char]+=1
+            else:
+                dict[char]=1
+        for i in t:
+           dict[i] = dict.get(i, 0) - 1
 
+           if dict[i] == -1:
+             return i
